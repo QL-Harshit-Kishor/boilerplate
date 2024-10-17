ed63de5b4f4a8b1e3b85e908bc7b4c1e81421d78
QL-Harshit-Kishor <harshit.kishor@quokkalabs.com>
2024-10-17 13:53:33 +0530

    [WIP]: navigation setup

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,11 +1,26 @@
-import {Text, View} from 'react-native';
 import React from 'react';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {useDeviceTheme} from '@app/hooks';
+import {navigationRef} from './NavigationService';
+import StackNavigator from './StackNavigator';
 
 const Navigator = () => {
+  // Get App Theme from settings
+  const deviceTheme = useDeviceTheme();
+  // Apply global theme (dark or light)
+  const theme = deviceTheme == 'dark' ? NavigationDarkTheme : NavigationDefaultTheme;
+
   return (
-    <View>
-      <Text>Navigator</Text>
-    </View>
+    <NavigationContainer
+      ref={navigationRef}
+      theme={theme}
+    >
+      <StackNavigator />
+    </NavigationContainer>
   );
 };
 
--- src/views/index.ts
+++ src/views/index.ts
@@ -3,3 +3,4 @@ export {default as HomeScreen} from './Home/HomeScreen';
 export {default as SettingScreen} from './Setting/SettingScreen';
 export {default as NetworkLoggerScreen} from './NetworkLogger/NetworkLoggerScreen';
 export {default as LoginScreen} from './Login/LoginScreen';
+export { default as WebviewScreen } from './Webview/WebviewScreen';
